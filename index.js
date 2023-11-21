9fb7fc9aeaf17969bfe6c060d1ba5ce73abac2ed
stf <sstefanofm@gmail.com>
2023-11-21 16:43:42 -0300

    cool foreground colors

--- index.js
+++ index.js
@@ -3,21 +3,19 @@ const random = (max = 10, min = 0) => {
 }
 
 const color = () => {
-  return random(48 - 30)
+  return random(256 - 0)
 }
 
 const lol = (...data) => {
   const loled = data
     .join(' ')
     .split('')
-    .map(c => `\x1b[${color()};1;${random(8)}m${c}\x1b[0m`)
+    .map(c => `\x1b[38;2;${color()};${color()};${color()}m${c}\x1b[0m`)
     .join('')
 
-  console.log(`${loled}\x1b[0m`)
+  console.log(loled)
 }
 
 console.lol = lol
 
 exports.console = console
-
-console.lol('asdasd', 1, 2, 3)
