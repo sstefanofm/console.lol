572b682f826ece8c4529fce55d47152e933b8c71
stf <sstefanofm@gmail.com>
2023-11-21 16:33:58 -0300

    supporting various colors

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const lol = (message = '') => {
-  const loled = message
+const color = () => {
+  return Math.floor(Math.random() * (48 - 30) + 30)
+}
+
+const lol = (...data) => {
+  const loled = data
+    .join(' ')
     .split('')
-    .map(c => `\x1b[31;1;${Math.floor(Math.random() * 10)}m${c}`)
+    .map(c => `\x1b[${color()};1;${Math.floor(Math.random() * 8)}m${c}`)
     .join('')
 
   console.log(`\x1b[31;1;4m${loled}\x1b[0m`)
