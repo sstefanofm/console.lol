54691096f1ff7872b1bc12ef413ff16341c912e6
stf <sstefanofm@gmail.com>
2023-11-21 15:48:43 -0300

    red but with different font styls

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const lol = message => {
-  console.log(`\x1b[31;1;4m${message}\x1b[0m`)
+const lol = (message = '') => {
+  const loled = message
+    .split('')
+    .map(c => `\x1b[31;1;${Math.floor(Math.random() * 10)}m${c}`)
+    .join('')
+
+  console.log(`\x1b[31;1;4m${loled}\x1b[0m`)
 }
 
 console.lol = lol
